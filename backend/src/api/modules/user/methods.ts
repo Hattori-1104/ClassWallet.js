5e153c62e70d50f01f06d429b1909e19424a35b1
Alloy-1104 <crafter.alloy@gmail.com>
2024-10-07 13:06:06 +0900

    🎨 認証アルゴリズムの改善

--- backend/src/api/modules/user/methods.ts
+++ backend/src/api/modules/user/methods.ts
@@ -1,7 +1,8 @@
-import { FieldPacket, ResultSetHeader } from "mysql2"
+import { FieldPacket, ResultSetHeader, RowDataPacket } from "mysql2"
 import con from "../database_connection"
 import { MethodResult } from "../result"
-import { RequestUser } from "./types"
+import { EmailSchema, RequestUser, RequestUserSchema, ResultUser, TagSchema } from "./types"
+import { Result } from "hono/router"
 
 const TABLE = "users"
 
@@ -20,4 +21,32 @@ async function register(user_data: RequestUser): Promise<MethodResult<{id: numbe
   }
 }
 
-export { register }
+// パスワードで認証
+async function verifyByPassword(user_identifier: RequestUser["email" | "tag"], password_hash: RequestUser["password_hash"]): Promise<MethodResult<{verified: boolean}>> {
+  // ユーザー識別子はメールアドレスかユーザータグのどちらか
+  let identifier_type: "email" | "tag"
+  if (EmailSchema.safeParse(user_identifier).success) {
+    identifier_type = "email"
+  } else if (TagSchema.safeParse(user_identifier).success) {
+    identifier_type = "tag"
+  } else {
+    return { type: "error", error: { code: "invalid_request", message: "Malformed request" }}
+  }
+  // クエリ文
+  const query = `SELECT 1 FROM ${TABLE} WHERE ${identifier_type} = :${identifier_type}`
+  const param = { [identifier_type]: user_identifier }
+  // クエリ
+  let result: ResultUser
+  let fields: FieldPacket[]
+  try {
+    [[result], fields] = await con.execute<ResultUser[]>(query, param)
+    if (!result) return { type: "error", error: { code: "invalid_request", message: "The requested user does not exist" }}
+  } catch (err: any) {
+    return { type: "error", error: { code: "database_error", message: err.message }}
+  }
+  // 認証
+  // ここから
+}
+
+
+export { register, verifyByPassword }
--- backend/src/api/modules/user/types.ts
+++ backend/src/api/modules/user/types.ts
@@ -1,28 +1,44 @@
+import { RowDataPacket } from "mysql2"
 import { z } from "zod"
 
 const EmailSchema = z.string().min(10).max(200).email()
 const TagSchema = z.string().min(2).max(100).startsWith("@").regex(/^@[0-9a-zA-Z_]+$/)
 const NameSchema = z.string().min(1).max(100)
+const PasswordHashSchema = z.string().length(64)
+const PasswordSaltScheam = z.string().length(8)
 
 const ResultUserSchema = z.object({
   id: z.number(),
   email: EmailSchema,
   tag: TagSchema,
   name: NameSchema,
-  password_hash: z.string().length(64),
-  password_salt: z.string().length(8),
-  updated_at: z.any()
+  password_hash: PasswordHashSchema,
+  password_salt: PasswordSaltScheam,
+  updated_at: z.number()
 })
 
 const RequestUserSchema = z.object({
   email: EmailSchema,
   tag: TagSchema,
   name: NameSchema,
-  password_hash: z.string().length(64),
-  password_salt: z.string().length(8),
+  password_hash: PasswordHashSchema,
+  password_salt: PasswordSaltScheam,
 })
 
-type ResultUser = z.infer<typeof ResultUserSchema>
+interface ResultUser extends RowDataPacket {
+  id: number,
+  email: string,
+  tag: string,
+  name: string,
+  password_hash: string,
+  password_salt: string,
+  updated_at: number,
+}
+
 type RequestUser = z.infer<typeof RequestUserSchema>
 
-export { ResultUser, RequestUser, ResultUserSchema, RequestUserSchema }
+export {
+  ResultUser, RequestUser,
+  ResultUserSchema, RequestUserSchema,
+  EmailSchema, TagSchema, NameSchema, PasswordHashSchema, PasswordSaltScheam
+}
